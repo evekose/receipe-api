2db4aa83a6ab8057f5f6fa14284e2ba6970a6b1f
amaasik <75305698+amaasik@users.noreply.github.com>
2023-01-26 14:03:13 +0200

    MariadbConnection

--- recipe-api/index.js
+++ recipe-api/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const app = require('express')()
+const mariadb = require('mariadb');
 const port = process.env.APP_PORT 
 
 const swaggerUI = require('swagger-ui-express')
@@ -8,9 +9,33 @@ const swaggerDocument= require('./docs/swagger.json')
 app.get('/recipe',(req, res) => {
     res.send(["Meatballs",["Vegetable soup"]])
 })
+
 app.use('/docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST, 
+    user:process.env.DB_USER, 
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+    connectionLimit: 5
+});
 
+app.get("/customers", async (req, res,)=>{
+    let connection 
+    try{
+        connection = await pool.getConnection()
+        const rows=await connection.query("SELECT * FROM customers")
+        console.log(rows)
+        res.send(rows)
+
+        
+    }catch(error){
+        throw error
+
+    }finally{
+        if(connection)return connection.end()
+    }
+})
 
 app.listen(port,()=>{
     console.log(`API up at: http://localhost:${port}`)
