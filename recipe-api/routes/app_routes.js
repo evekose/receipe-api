a8ed7ed0e6ba0a2b1524f84af17dd2ad5a8910d3
amaasik <75305698+amaasik@users.noreply.github.com>
2023-02-09 16:05:42 +0200

    Add delete functionality [delivers #184268238]

--- recipe-api/routes/app_routes.js
+++ recipe-api/routes/app_routes.js
@@ -13,9 +13,9 @@ module.exports = (app) => {
 module.exports = (app) => {
 app.route("/ingredients")
 .get(ingredientsController.getAll)
-//     .post(ingredientsController.createNew)    // Create
+.post(ingredientsController.createNew)    // Create
 app.route("/ingredients/:id")
 .get(ingredientsController.getById)       // Read
 //     .put(ingredientsController.updateById)    // Update
-//     .delete(ingredientsController.deleteById) // Delete
+.delete(ingredientsController.deleteById) // Delete
 }
