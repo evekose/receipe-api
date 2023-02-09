21674ec29e4f84e856779858932b8d3e58f789a2
amaasik <75305698+amaasik@users.noreply.github.com>
2023-02-09 15:03:41 +0200

    IngredientErrorMessage

--- recipe-api/controllers/IngredientsController.js
+++ recipe-api/controllers/IngredientsController.js
@@ -15,7 +15,29 @@ exports.getById = async (req, res) => {
     }
     res.send(ingredients)
 }
-exports.createNew = async (req,res) => {
-    console.log(req.body)
-    res.send(req.body)
-}
+exports.createNew = async (req, res) => {
+    let ingredient
+    try {
+        ingredient = await Ingredient.create(req.body)
+    } catch (error) {
+      if (error instanceof db.Sequelize.ValidationError) {
+        res.status(400).send({"error":error.errors.map((item)=> item.message)})
+      } else {
+        console.log("IngredientsCreate: ",error)
+        res.status(500).send({"error":"Something went wrong on our side. Sorry :("})
+      }
+      return
+    }
+    res
+      .status(201)
+      .location(`${getBaseUrl(req)}/ingredients/${ingredient.id}`)
+      .json(ingredient)
+  }
+  
+  getBaseUrl = (request) => {
+    return (
+      (request.connection && request.connection.encrypted ? "https" : "http") +
+      `://${request.headers.host}`
+    )
+  }
+  
